d1c2177a43abf792bd8c626414e89594180876ce
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-02-11 11:49:11 -0600

    created fetch call to backend and got all adventures page to populate the data received

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
 import './App.css';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 //COMPONENTS
 import AllAdventures from './components/AllAdventures';
@@ -8,8 +9,6 @@ import AdventureDetails from './components/AdventureDetails';
 import NewAdventure from './components/NewAdventure';
 import Search from './components/Search';
 import SearchDetails from './components/SearchDetails';
-
-//FUNCTIONS
 function HomePage(){
   return(<>
     <h1>Home Page</h1>
@@ -19,12 +18,40 @@ function HomePage(){
   </>)
 }
 
+
+
 function App() {
+  //MAIN DATA STATE
+  const [adventures, setAdventures] = useState([])
+
+  //FUNCTIONS
+  
+  //Fetch all present data on load
+  
+  const handleFetch = async () => {
+    //URL will have to be the heroku address that I deploy the backend to
+    const URL = "http://localhost:8000/adventures/"
+    fetch(URL).then(resp => {
+      return resp.json()
+      .then(data => {
+        console.log('handlefetch', data)
+        setAdventures(data)
+      })
+    })
+  }
+
+  //HandleFetch function call
+  useEffect(() => {
+    handleFetch()
+
+    
+  }, [])
+  console.log('current main data state', adventures)
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/adventures' element={<AllAdventures />} />
+        <Route path='/adventures' element={<AllAdventures adventures={adventures} />} />
         <Route path='/adventures/new' element={<NewAdventure />} />
         <Route path='/adventures/:id' element={<AdventureDetails />} />
         <Route path='/search' element={<Search />} />
--- src/components/AllAdventures.jsx
+++ src/components/AllAdventures.jsx
@@ -1,9 +1,22 @@
+import {Link} from 'react-router-dom'
 
-
-function AllAdventures(){
-
-    return(<div>
+function AllAdventures(props){
+    const {adventures} = props
+console.log('all adventures props', props)
+    return(<div className="adventures-page">
         <h1>All Adventures</h1>
+        <div className="adventures-list">
+            {adventures && adventures.map((adventure, index) => (
+                <div className="individual-adventure" key={adventure._id}>
+                    <Link to={`/adventures/${adventure._id}`}>{adventure.name}</Link>
+                </div>
+            ))}
+        </div>
+
+
+        {/* <h2>{adventures[0].name}</h2>
+        <p>Stop 1: {adventures[0].stops[0].name}</p>
+        <p>{adventures[0].stops[0].description}</p> */}
         
     </div>)
 }
