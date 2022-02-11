e76e07b3fd8a49abbe74b8971f77ba6564a5788c
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-02-10 20:43:12 -0600

    added form to new adventure page

--- planning.md
+++ planning.md
@@ -4,5 +4,7 @@
     AllAdventures
     AdventureDetails
     NewAdventure
+        MPV - input for "title", "stop name" and "description, all in one form.
+        STRETCH - + button that adds a new input for a new stop...possibly array of "inputs" and + button adds one to the array...they are all linked to submit and will send data no matter how many fields are created.
     Search
     SearchDetails
--- src/components/NewAdventure.jsx
+++ src/components/NewAdventure.jsx
@@ -1,9 +1,20 @@
 
 
-function NewAdventure(){
+function NewAdventure() {
 
-    return(<div>
-        <h1>New Adventure</h1>
+    return (<div>
+        <h1>New Adventure Plan</h1>
+
+        <form>
+            <label htmlFor="title">Plan Name:</label>
+            <input id="title" type="text" placeholder="New Adventure Title" />
+            <label htmlFor="stop">New Stop Title:</label>
+            <input id="stop" type="text" placeholder="New Stop Name" />
+            <label htmlFor="description">Stop Description:</label>
+            <textarea name="" id="description" cols="30" rows="10"></textarea>
+            <button>Create New Plan</button>
+        </form>
+
     </div>)
 }
 
