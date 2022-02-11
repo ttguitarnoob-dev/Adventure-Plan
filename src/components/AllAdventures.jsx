904cdaa50fb097547f9ab0951f2d2115e035fda4
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-02-11 11:52:11 -0600

    added id to each iteration of individual adventure

--- src/components/AllAdventures.jsx
+++ src/components/AllAdventures.jsx
@@ -7,17 +7,11 @@ console.log('all adventures props', props)
         <h1>All Adventures</h1>
         <div className="adventures-list">
             {adventures && adventures.map((adventure, index) => (
-                <div className="individual-adventure" key={adventure._id}>
+                <div className="individual-adventure" key={adventure._id} id={`adventure-${index}`}>
                     <Link to={`/adventures/${adventure._id}`}>{adventure.name}</Link>
                 </div>
             ))}
         </div>
-
-
-        {/* <h2>{adventures[0].name}</h2>
-        <p>Stop 1: {adventures[0].stops[0].name}</p>
-        <p>{adventures[0].stops[0].description}</p> */}
-        
     </div>)
 }
 
