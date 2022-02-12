91d33edf2f7ddba141affc76d56fb5ba5fce2ded
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-02-12 17:42:13 -0600

    added functions to call api for gps coordinates based on a search term, and change those coordinates into another api call that pulls up interesting locations near the coordinates. It works

--- src/App.js
+++ src/App.js
@@ -29,6 +29,36 @@ function App() {
   
 
   //FUNCTIONS
+
+  //API Fetching
+ 
+    async function GrabCoordinates(search){
+      const URL = `https://api.opentripmap.com/0.1/en/places/geoname?name=${search}&country=US&apikey=${process.env.REACT_APP_OPEN_TRIP_API_KEY}`
+      const options = {
+        method: "GET"
+      }
+      console.log('searched URL isssisis', URL)
+  
+      const response = await fetch(URL, options)
+      const coordinates = await response.json()
+      console.log('coordinates', coordinates)
+      LocationResults(coordinates)
+    }
+
+
+
+    async function LocationResults(coordinates){
+    const URL = `https://api.opentripmap.com/0.1/en/places/radius?radius=500&lon=${coordinates.lon}&lat=${coordinates.lat}&limit=30&apikey=${process.env.REACT_APP_OPEN_TRIP_API_KEY}`
+    const options = {
+      method: "GET"
+    }
+    console.log('searched lon lat url is', URL)
+    const response = await fetch(URL, options)
+    const results = await response.json()
+    console.log('final search results', results.features)
+  }
+
+  
   
   //Fetch all present data on load
   
@@ -74,7 +104,7 @@ function App() {
         <Route path='/adventures' element={<AllAdventures adventures={adventures} />} />
         <Route path='/adventures/new' element={<NewAdventure adventures={adventures} setAdventures={setAdventures}/>} />
         <Route path='/adventures/:id' element={<AdventureDetails DeleteAdventure={DeleteAdventure}/>} />
-        <Route path='/search' element={<Search />} />
+        <Route path='/search' element={<Search GrabCoordinates={GrabCoordinates}/>} />
         <Route path='/search/:id' element={<SearchDetails />} />
         <Route path='/adventures/update/:id' element={<AddStop />} />
         
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react'
 
 
 
-function Search(){
 
-    return(<div>
+function Search(props) {
+const {GrabCoordinates} = props
+
+function HandleSubmit(e){
+    e.preventDefault()
+    const denver = "Denver"
+
+    GrabCoordinates(denver)
+
+}
+    return (<div>
         <h1>Search</h1>
+        <form onSubmit={HandleSubmit}>
+            <button>Search for Denver</button>
+        </form>
+
     </div>)
 }
 
